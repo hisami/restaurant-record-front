ae149e2f918dbca1fbcf799b58f1e8a6e72b000a
hisamitsu <ryohei.hisamitsu@ncdc.co.jp>
2021-08-14 12:03:14 +0900

    tailwindの導入

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import tailwind from "tailwind-rn";
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={tailwind("text-blue-800")}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
