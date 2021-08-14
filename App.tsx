0d032f502245f582eaa6d01acaea6069fa8d35b4
hisamitsu <ryohei.hisamitsu@ncdc.co.jp>
2021-08-14 19:43:04 +0900

    レイアウト一旦作成

--- App.tsx
+++ App.tsx
@@ -1,26 +1,45 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  ScrollView,
+  Button,
+} from "react-native";
 import tailwind from "tailwind-rn";
 
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={tailwind("text-blue-800")}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={tailwind("h-full")}>
+      <View style={tailwind("p-6 h-full")}>
+        <Text style={tailwind("text-3xl font-bold text-center")}>
+          行きたいお店
+        </Text>
+        <TextInput
+          style={tailwind("p-4 my-4 h-16 text-xl bg-gray-100 rounded-full")}
+          placeholder="検索"
+        />
+        <ScrollView style={tailwind("flex flex-col")}>
+          <View style={tailwind("flex flex-row my-4")}>
+            <View style={tailwind("bg-gray-200 w-20 h-20 mr-4 rounded")} />
+            <View style={tailwind("flex flex-col justify-between")}>
+              <Text style={tailwind("text-lg font-bold")}>お店の名前</Text>
+              <Text style={tailwind("text-base")}>広島県広島市西区</Text>
+            </View>
+          </View>
+        </ScrollView>
+        <View style={tailwind("my-4 h-10")}>
+          <Button
+            onPress={() => {
+              console.log("test");
+            }}
+            title="近くのお店を検索して追加"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
